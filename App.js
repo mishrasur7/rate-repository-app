35a28a285b1549f453da676cfad18862f872400d
Suraj Mishra <mishrasur7@gmail.com>
2023-07-11 16:30:24 +0300

    Part10 exercise 10.3 completed

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RepositoryList from './components/RepositoryList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <RepositoryList />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/RepositoryItem.jsx
@@ -0,0 +1,26 @@
+import { Text, View, StyleSheet } from "react-native"
+import { StatusBar } from 'expo-status-bar';
+
+export const RepositoryItem = ({ item }) => {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.item}>Full Name: {item.fullName}</Text>
+        <Text style={styles.item}>Description: {item.description}</Text>
+        <Text style={styles.item}>Language: {item.language}</Text>
+        <Text style={styles.item}>Stars: {item.stargazersCount}</Text>
+        <Text style={styles.item}>Forks: {item.forksCount}</Text>
+        <Text style={styles.item}>Reviews: {item.reviewCount}</Text>
+        <Text style={styles.item}>Ratings: {item.ratingAverage}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        fontSize: 20
+    }
+})
