6c96c7fdd912be09e23c5e5b53574e2dd3abcb53
Suraj Mishra <mishrasur7@gmail.com>
2023-07-12 10:47:53 +0300

    Main component added

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import RepositoryList from './components/RepositoryList';
+import Main from './components/Main';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <RepositoryList />
+      <Main />
       <StatusBar style="auto" />
     </View>
   );
