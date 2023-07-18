3aca27e8dc41ad3f09b624b330eef0c87dfcff45
Suraj Mishra <mishrasur7@gmail.com>
2023-07-18 17:46:52 +0300

    Part10 exercise 10.10 completed

--- src/theme.js
+++ src/theme.js
@@ -1,15 +1,24 @@
+import { Platform, Switch } from "react-native"
+
 const theme = {
     colors: {
         appBarBackgroundColor: "#24292e",
-        tabTextColor: 'white',
-        button: '#1e90ff',
+        tabTextColor: "white",
+        button: "#1e90ff",
         errorInput: "#d73a4a"
     },
     fontSizes: {
         tab: 25
     },
     fontWeights: {
-        tab: 'bold'
+        tab: "bold"
+    },
+    fonts: {
+        main: Platform.select({
+            ios: "Arial",
+            android: "Roboto",
+            default: "System"
+        })
     }
 }
 
