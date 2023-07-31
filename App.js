3a14d392a76528ede569ba73fa97f2d50fefc0b9
Suraj Mishra <mishrasur7@gmail.com>
2023-07-31 12:01:57 +0300

    apollo client installed and configured

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-import { View } from 'react-native';
+import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client';
 
 import Main from './src/components/Main';
-import { NativeRouter } from 'react-router-native';
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient(); 
 
 export default function App() {
   return (
-    <View>
       <NativeRouter>
-        <Main />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
-    </View>
   );
 }
