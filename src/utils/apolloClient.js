3d0593ec9e9157f7136d8e389ad97c914678b71c
Suraj Mishra <mishrasur7@gmail.com>
2023-08-03 14:38:18 +0300

    Part10 exercise 10.12 completed

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants'
 
 const httpLink = createHttpLink({
-  uri: 'http://10.46.21.192:4000/graphql',
+  uri: Constants.manifest.extra.env
 });
 
 const createApolloClient = () => {
